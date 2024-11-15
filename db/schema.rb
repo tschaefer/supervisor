ba7b0a9cf19c8408a458bd75709986888889a578
tschaefer <github@blackox.org>
2024-11-15 11:37:40 +0100

    Maintenance: Migrate db schemas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_30_141137) do
+ActiveRecord::Schema[8.0].define(version: 2024_09_30_141137) do
   create_table "stacks", force: :cascade do |t|
     t.string "name", null: false
     t.string "uuid", null: false
