54473b56bd6be9b4dad0a05cc68854589152be22
tschaefer <github@blackox.org>
2024-12-22 09:46:46 +0100

    Add spec tests
    skip-checks:true

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Dashboard', type: :request do
-  describe 'GET /dashboard' do
-    def basic_auth(username: nil, password: nil)
-      user = username.presence || DashboardController.username
-      pass = password.presence || DashboardController.password
-      ActionController::HttpAuthentication::Basic.encode_credentials(user, pass)
-    end
+  def basic_auth(username: nil, password: nil)
+    user = username.presence || DashboardController.username
+    pass = password.presence || DashboardController.password
+    ActionController::HttpAuthentication::Basic.encode_credentials(user, pass)
+  end
 
+  describe 'Authorization' do
     context 'with no credentials' do
       it 'returns http unauthorized' do
         get '/dashboard'
@@ -28,13 +28,13 @@ RSpec.describe 'Dashboard', type: :request do
         expect(response).to have_http_status(:success)
       end
     end
+  end
 
-    context 'with stack UUID parameter' do
-      it 'stores UUID in DOM' do
-        stack_uuid = SecureRandom.uuid
-        get "/dashboard?uuid=#{stack_uuid}", headers: { 'HTTP_AUTHORIZATION' => basic_auth }
-        expect(response.body).to include(stack_uuid)
-      end
+  context 'with stack UUID parameter' do
+    it 'stores UUID in DOM' do
+      stack_uuid = SecureRandom.uuid
+      get "/dashboard?uuid=#{stack_uuid}", headers: { 'HTTP_AUTHORIZATION' => basic_auth }
+      expect(response.body).to include(stack_uuid)
     end
   end
 end
