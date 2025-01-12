1eeb0d56ac2cfaf94e4c492f26df3ae7109152f3
tschaefer <github@blackox.org>
2025-01-12 19:02:43 +0100

    Add host info in dashboard

--- app/helpers/host_info_helper.rb
+++ app/helpers/host_info_helper.rb
@@ -3,9 +3,9 @@ module HostInfoHelper
     server_name = request.server_name
 
     info = Rails.cache.fetch(server_name, expires_in: 1.day) do
-      Supervisor::HostInfo.new(server_name).to_h
+      Supervisor::HostInfo.new(server_name)
     end
 
-    "Hostname: #{info[:hostname]} | Address: #{info[:ip]} | Location: #{info[:location]}"
+    "Hostname: #{info.hostname} | Address: #{info.ip} | Location: #{info.location}"
   end
 end
