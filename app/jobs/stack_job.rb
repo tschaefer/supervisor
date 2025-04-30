cdadbb53e952df302a6526add87c96cae8db61d9
Tobias Schäfer <github@blackox.org>
2025-04-30 20:00:40 +0200

    Maintenance: Lock stack job runs (#64)

--- app/jobs/stack_job.rb
+++ app/jobs/stack_job.rb
@@ -5,17 +5,34 @@ class StackJob < ApplicationJob
   STACKS_ROOT = ENV.fetch('SUPERVISOR_STACKS_ROOT', Rails.root.join('storage/stack'))
 
   def perform(stack)
-    # On destroy, the relevant stack attributes are passed as a hash
     @stack = stack.is_a?(Hash) ? Hashie::Mash.new(stack) : stack
 
     include_files = @stack.compose_includes.join(' ')
-    @assets = @stack.assets
-                    .merge(include_files:)
+    @assets = @stack.assets.merge(include_files:)
+
+    return if !instance_of?(StackDestroyJob) && !lock_for_stack!
 
     execute
   end
 
   private
+
+  def lock_for_stack!
+    lock_file = File.join(@assets.base_dir.to_s, '.lock')
+    FileUtils.mkdir_p(@assets.base_dir)
+
+    File.open(lock_file, File::RDWR | File::CREAT) do |f|
+      unless f.flock(File::LOCK_EX | File::LOCK_NB)
+        Rails.logger.info("StackJob: lock held for #{@stack.uuid}, exiting early")
+        return false
+      end
+      f.flock(File::LOCK_UN)
+    end
+
+    true
+  rescue StandardError => e
+    Rails.logger.error("StackJob: lock check failed: #{e.message}")
+  end
 
   def execute
     script = ERB.new(
